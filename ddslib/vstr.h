7d57457e3dc1b365f5d93a4ccea6f52f0ce7386e
Steven Simpson <568737+simpsonst@users.noreply.github.com>
2016-04-15 13:26:46 +0000

    Filled out and fixed dynamic string functions.

--- ddslib/vstr.h
+++ ddslib/vstr.h
@@ -20,16 +20,14 @@
     Author contact: Email to ss@comp.lancs.ac.uk
 */
 
-#ifndef vstr_INCLUDED
-#define vstr_INCLUDED
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+#ifndef vstr_INCLUDED
+#define vstr_INCLUDED
+
 #include <stddef.h>
-#include <string.h>
-#include <stdarg.h>
 
   typedef struct {
     char *base;
@@ -38,77 +36,190 @@ extern "C" {
 
 #define vstr_NULL ((vstr) { NULL, 0, 0 })
 
-#if defined __GNUC__ && !defined __GNUC_STDC_INLINE__
-#define vstr_inline extern inline
-#define vstr_INLINEBODY(B) B struct tm
-#else
-#define vstr_inline inline
-#define vstr_INLINEBODY(B)
-#endif
+  /* Access underlying array. */
+  char *vstr_get(const vstr *p);
+
+  /* Get used length. */
+  size_t vstr_len(const vstr *p);
+
+  /* Detach and return allocated array. */
+  char *vstr_extract(vstr *p);
+
+  /* Set the capacity to match the length. */
+  void vstr_compact(vstr *p);
+
+  /* Ensure string is null-terminated. */
+  int vstr_term(vstr *p);
+
+  /* Ensure string is not null-terminated. */
+  int vstr_unterm(vstr *p);
+
+  /* Set the capacity, or fail if truncating would occur. */
+  int vstr_setcap(vstr *p, size_t cap);
+
+  /* Ensure minimum capacity. */
+  int vstr_ensure(vstr *p, size_t cap);
+
+
+
+
+  /* Zeroing functions */
+
+  void vstr_clear(vstr *p); // Set non-null string to empty.
+  int vstr_empty(vstr *p); // Set to empty.
+  void vstr_reset(vstr *p); // Set to null.
+
+
+  /* ~: Use all of null-terminated string, except terminator.
+
+     ~0: Use all of null-terminated string, including terminator.
+
+     ~n: Use array of known size.
+
+     ~v: Use all of other managed string.
+
+     ~vi: Use all but head of other managed string.
+
+     ~vn: Use head of other managed string.
+
+     ~vin: Use middle of other managed string.
+
+     ~vr: Use tail of other managed string.
+
+     ~vrn: Use middle of other managed string, reversed with
+     indexing.
+
+     ~f: Use format string and corresponding arguments.
+
+     v~f: Use format string and argument list. */
+
+
+  /* Assignment functions: Any previous value is discarded. */
+
+  int vstr_set(vstr *p, const char *s);
+  int vstr_set0(vstr *p, const char *s);
+  int vstr_setn(vstr *p, const char *, size_t n);
+  int vstr_setc(vstr *p, int, size_t n);
+  int vstr_setv(vstr *p, const vstr *q);
+  int vstr_setvi(vstr *p, const vstr *q, size_t qx);
+  int vstr_setvn(vstr *p, const vstr *q, size_t qn);
+  int vstr_setvin(vstr *p, const vstr *q, size_t qx, size_t qn);
+  int vstr_setvr(vstr *p, const vstr *q, size_t qx);
+  int vstr_setvrn(vstr *p, const vstr *q, size_t qx, size_t qn);
+  int vstr_setf(vstr *p, const char *fmt, ...);
+
+
+
+
+  /* Insertion functions */
+  int vstr_insert(vstr *p, size_t x, const char *s);
+  int vstr_insert0(vstr *p, size_t x, const char *s);
+  int vstr_insertc(vstr *p, size_t x, int, size_t n);
+  int vstr_insertn(vstr *p, size_t x, const char *, size_t n);
+  int vstr_insertv(vstr *p, size_t x, const vstr *q);
+  int vstr_insertvi(vstr *p, size_t x, const vstr *q, size_t qx);
+  int vstr_insertvn(vstr *p, size_t x, const vstr *q, size_t qn);
+  int vstr_insertvin(vstr *p, size_t x,
+		     const vstr *q, size_t qx, size_t qn);
+  int vstr_insertvr(vstr *p, size_t x, const vstr *q, size_t qx);
+  int vstr_insertvrn(vstr *p, size_t x,
+		     const vstr *q, size_t qx, size_t qn);
+  int vstr_insertf(vstr *p, size_t x, const char *fmt, ...);
+
+
+  /* Appendage functions; All of these are equivalent to their
+     insertion counterparts, but with x set to the length. */
+  int vstr_append(vstr *p, const char *s);
+  int vstr_append0(vstr *p, const char *s);
+  int vstr_appendn(vstr *p, const char *s, size_t n);
+  int vstr_appendc(vstr *p, int c, size_t n);
+  int vstr_appendv(vstr *p, const vstr *q);
+  int vstr_appendvi(vstr *p, const vstr *q, size_t qx);
+  int vstr_appendvn(vstr *p, const vstr *q, size_t qn);
+  int vstr_appendvin(vstr *p, const vstr *q, size_t qx, size_t qn);
+  int vstr_appendvr(vstr *p, const vstr *q, size_t qx);
+  int vstr_appendvrn(vstr *p, const vstr *q, size_t qx, size_t qn);
+  int vstr_appendf(vstr *p, const char *fmt, ...);
+
+
+  /* splice/elide/elect functions have the following forms:
+
+     ~: x is index from start, n counts towards end.
+
+     r~: x is index from end, n counts towards start.
+
+     ~r: x is index from end, n counts towards end. 
+
+     r~r: x is index from start, n counts towards start. */
+
+
+
+  /* Internal allocation functions */
+
+  /* Insert space for n elements at x from start, returning pointer to
+     start of new space. */
+  char *vstr_splice(vstr *p, size_t x, size_t n);
+
+  /* Insert space for n elements at x from end, returning pointer to
+     one-past-end of new space. */
+  char *vstr_rsplice(vstr *p, size_t x, size_t n);
+
+  /* Insert space for n elements at x from end, returning pointer to
+     start of new space. */
+  char *vstr_splicer(vstr *p, size_t x, size_t n);
+
+  /* Insert space for n elements at x from start, returning pointer to
+     one-past-end of new space. */
+  char *vstr_rsplicer(vstr *p, size_t x, size_t n);
+
+  /* Remove a central piece. */
+  void vstr_elide(vstr *p, size_t x, size_t n);
+  void vstr_relide(vstr *p, size_t x, size_t n);
+  void vstr_elider(vstr *p, size_t x, size_t n);
+  void vstr_relider(vstr *p, size_t x, size_t n);
+
+  /* Retain a central piece. */
+  void vstr_elect(vstr *p, size_t x, size_t n);
+  void vstr_relect(vstr *p, size_t x, size_t n);
+  void vstr_electr(vstr *p, size_t x, size_t n);
+  void vstr_relectr(vstr *p, size_t x, size_t n);
+
+  /* Keep the first n characters. */
+  void vstr_truncate(vstr *p, size_t n);
+
+  /* Remove the last n characters. */
+  void vstr_rtruncate(vstr *p, size_t n);
+
+  /* Remove the first n characters. */
+  void vstr_neck(vstr *p, size_t n);
+
+  /* Keep the last n characters. */
+  void vstr_rneck(vstr *p, size_t n);
+
+
 
   int vstr_wcsmblen(const wchar_t *s, size_t len);
 
-  vstr_inline char *vstr_get(const vstr *p) { return p->base; }
-  vstr_inline size_t vstr_len(const vstr *p) { return p->len; }
-  vstr_inline void vstr_clear(vstr *p) { p->len = 0; }
+#define vstr_get(p) ((char *) (p)->base)
+#define vstr_len(p) ((size_t) (p)->len)
+#define vstr_clear(p) ((void) ((p)->len = 0))
 
-  char *vstr_extract(vstr *p);
-  int vstr_empty(vstr *p);
-  void vstr_reset(vstr *);
-  void vstr_compact(vstr *);
-  int vstr_term(vstr *);
-  int vstr_unterm(vstr *);
-  int vstr_setcap(vstr *p, size_t nc);
-  int vstr_ensure(vstr *p, size_t cap);
-  char *vstr_splice(vstr *, size_t index, size_t n);
-  int vstr_insertf(vstr *, size_t index, const char *fmt, ...);
-  int vstr_vinsertf(vstr *, size_t index, const char *fmt, va_list ap);
-  int vstr_insertc(vstr *, size_t index, int, size_t);
-  int vstr_insertn(vstr *, size_t index, const char *, size_t);
-  int vstr_insertvin(vstr *p, size_t index,
-		     const vstr *q, size_t qi, size_t qn);
-  int vstr_insertvrn(vstr *p, size_t index,
-		     const vstr *q, size_t qi, size_t qn);
-  int vstr_insertvi(vstr *p, size_t index, const vstr *q, size_t qi);
-  int vstr_insertvn(vstr *p, size_t index, const vstr *q, size_t qn);
-  int vstr_insertvr(vstr *p, size_t index, const vstr *q, size_t qi);
-  int vstr_insertv(vstr *p, size_t index, const vstr *q);
-  void vstr_elide(vstr *, size_t index, size_t);
-  void vstr_relide(vstr *, size_t index, size_t);
-  void vstr_truncate(vstr *, size_t index);
-  void vstr_rtruncate(vstr *, size_t index);
-  void vstr_elect(vstr *p, size_t index, size_t n);
-  void vstr_relect(vstr *p, size_t index, size_t n);
-  int vstr_appendf(vstr *p, const char *fmt, ...);
-  int vstr_appendvin(vstr *p, const vstr *q, size_t qi, size_t qn);
-  int vstr_appendvrn(vstr *p, const vstr *q, size_t qi, size_t qn);
-  int vstr_appendvi(vstr *p, const vstr *q, size_t qi);
-  int vstr_appendvn(vstr *p, const vstr *q, size_t qn);
-  int vstr_appendvr(vstr *p, const vstr *q, size_t qi);
-  int vstr_appendv(vstr *p, const vstr *q);
+  char *(vstr_get)(const vstr *p);
+  size_t (vstr_len)(const vstr *p);
+  void (vstr_clear)(vstr *p);
 
-  vstr_inline int vstr_vappendf(vstr *p, const char *fmt, va_list ap) {
-    return vstr_vinsertf(p, vstr_len(p), fmt, ap);
-  }
+#endif
 
-  vstr_inline int vstr_insert(vstr *p, size_t index, const char *s) {
-    return vstr_insertn(p, index, s, strlen(s));
-  }
-
-  vstr_inline int vstr_appendn(vstr *p, const char *s, size_t n) {
-    return vstr_insertn(p, vstr_len(p), s, n);
-  }
-
-  vstr_inline int vstr_appendc(vstr *p, int c, size_t n) {
-    return vstr_insertc(p, vstr_len(p), c, n);
-  }
-
-  vstr_inline int vstr_append(vstr *p, const char *s) {
-    return vstr_appendn(p, s, strlen(s));
-  }
+#ifdef va_start
+  /* We can afford to re-declare functions without re-inclusion
+     protection.  This allows the user to include <stdarg.h> and then
+     <ddslib/vstr.h> again to access these specific functions if an
+     earlier inclusion failed to declare them. */
+  int vstr_vsetf(vstr *p, const char *fmt, va_list ap);
+  int vstr_vinsertf(vstr *p, size_t x, const char *fmt, va_list ap);
+  int vstr_vappendf(vstr *p, const char *fmt, va_list ap);
+#endif
 
 #ifdef __cplusplus
 }
-#endif
-
 #endif
--- testvstr.c
+++ testvstr.c
@@ -1,35 +1,428 @@
+#undef NDEBUG
+
 #include <stdio.h>
+#include <string.h>
 #include <assert.h>
+#include <stdlib.h>
 
 #include "ddslib/vstr.h"
 
+static void check(int line, const vstr *p, const char *s, size_t len)
+{
+  if (vstr_len(p) != len - 1 || memcmp(vstr_get(p), s, len - 1)) {
+    fprintf(stderr, "%4d EXPECTED: %zu [%.*s]\n",
+	    line, len - 1, (int) (len - 1), s);
+    fprintf(stderr, "          GOT: %zu [%.*s]\n",
+	    vstr_len(p), (int) vstr_len(p), vstr_get(p));
+    fflush(stderr);
+    abort();
+  }
+}
+
+#define CHECK(P, S) check(__LINE__, (P), (S), sizeof (S))
+
 int main(void)
 {
-  char temp[10];
-  snprintf(temp, sizeof temp, "Madam, I'm Adam.");
-  printf("Last character is %d.\n", temp[9]);
+  vstr s1 = vstr_NULL;
 
-  int rc;
-  vstr s1 = vstr_NULL, s2 = vstr_NULL;
+  assert(vstr_get(&s1) == NULL);
+  assert(vstr_len(&s1) == 0);
+  assert(vstr_extract(&s1) == NULL);
 
-  rc = vstr_append(&s1, "Happy banana!"), assert(rc == 0);
-  rc = vstr_append(&s2, "Slappy bunny."), assert(rc == 0);
-  rc = vstr_insertv(&s1, 6, &s2), assert(rc == 0);
+  {
+    vstr_setc(&s1, 'z', 10);
+    CHECK(&s1, "zzzzzzzzzz");
+  }
 
-  printf("s1 = %.*s\n", (int) vstr_len(&s1), vstr_get(&s1));
+  {
+    vstr_truncate(&s1, 6);
+    CHECK(&s1, "zzzzzz");
+  }
 
-  vstr_elide(&s1, 4, 5);
+  {
+    char *p = vstr_splice(&s1, 3, 10);
+    assert(p == vstr_get(&s1) + 3);
+    assert(vstr_len(&s1) == 16);
+    assert(memcmp(vstr_get(&s1), "zzz", 3) == 0);
+    assert(memcmp(vstr_get(&s1) + 13, "zzz", 3) == 0);
+  }
 
-  printf("s1 = %.*s\n", (int) vstr_len(&s1), vstr_get(&s1));
+  {
+    vstr_elide(&s1, 3, 10);
+    CHECK(&s1, "zzzzzz");
+  }
 
-  rc = vstr_insertf(&s1, 8, "conk %ls here ", L"Degenerate"), assert(rc == 0);
+  {
+    int rc = vstr_insertc(&s1, 3, 'a', 7);
+    assert(rc == 0);
+    CHECK(&s1, "zzzaaaaaaazzz");
+  }
 
-  printf("s1 = %.*s\n", (int) vstr_len(&s1), vstr_get(&s1));
+  {
+    int rc = vstr_insert(&s1, 5, "boo");
+    assert(rc == 0);
+    CHECK(&s1, "zzzaabooaaaaazzz");
+  }
 
-  rc = vstr_insertf(&s1, 100, "flangey"), assert(rc == 0);
+  {
+    int rc = vstr_setf(&s1, "%dx%d", 21, 34);
+    assert(rc == 0);
+    CHECK(&s1, "21x34");
+  }
 
-  printf("s1 = %.*s\n", (int) vstr_len(&s1), vstr_get(&s1));
-  assert(vstr_get(&s1)[vstr_len(&s1) - 1] != '\0');
+  {
+    int rc = vstr_set0(&s1, "worst");
+    assert(rc == 0);
+    CHECK(&s1, "worst\0");
+  }
+
+  vstr s2 = vstr_NULL;
+  assert(vstr_get(&s2) == NULL);
+  assert(vstr_len(&s2) == 0);
+  assert(vstr_extract(&s2) == NULL);
+
+  {
+    int rc = vstr_set(&s2, "bing-bong");
+    assert(rc == 0);
+    CHECK(&s2, "bing-bong");
+  }
+
+
+
+  {
+    int rc = vstr_setv(&s1, &s2);
+    assert(rc == 0);
+    CHECK(&s1, "bing-bong");
+  }
+
+  {
+    int rc = vstr_setvi(&s1, &s2, 2);
+    assert(rc == 0);
+    CHECK(&s1, "ng-bong");
+  }
+
+  {
+    int rc = vstr_setvi(&s1, &s2, 12);
+    assert(rc == 0);
+    CHECK(&s1, "");
+  }
+
+  {
+    int rc = vstr_setvn(&s1, &s2, 5);
+    assert(rc == 0);
+    CHECK(&s1, "bing-");
+  }
+
+  {
+    int rc = vstr_setvn(&s1, &s2, 12);
+    assert(rc == 0);
+    CHECK(&s1, "bing-bong");
+  }
+
+  {
+    int rc = vstr_setvin(&s1, &s2, 1, 2);
+    assert(rc == 0);
+    CHECK(&s1, "in");
+  }
+
+  {
+    int rc = vstr_setvr(&s1, &s2, 5);
+    assert(rc == 0);
+    CHECK(&s1, "-bong");
+  }
+
+  {
+    int rc = vstr_setvr(&s1, &s2, 20);
+    assert(rc == 0);
+    CHECK(&s1, "bing-bong");
+  }
+
+  {
+    int rc = vstr_setvrn(&s1, &s2, 5, 3);
+    assert(rc == 0);
+    CHECK(&s1, "-bo");
+  }
+
+  {
+    int rc = vstr_setvrn(&s1, &s2, 3, 5);
+    assert(rc == 0);
+    CHECK(&s1, "ong");
+  }
+
+  {
+    int rc = vstr_setvrn(&s1, &s2, 12, 5);
+    assert(rc == 0);
+    CHECK(&s1, "bi");
+  }
+
+  {
+    int rc = vstr_setvrn(&s1, &s2, 12, 2);
+    assert(rc == 0);
+    CHECK(&s1, "");
+  }
+
+
+
+
+
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_elide(&s1, 3, 3);
+    CHECK(&s1, "hoo-flip");
+  }
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_elide(&s1, 3, 10);
+    CHECK(&s1, "hoo");
+  }
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_elide(&s1, 12, 10);
+    CHECK(&s1, "hoojar-flip");
+  }
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_relide(&s1, 3, 3);
+    CHECK(&s1, "hoojalip");
+  }
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_relide(&s1, 3, 10);
+    CHECK(&s1, "lip");
+  }
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_relide(&s1, 12, 10);
+    CHECK(&s1, "hoojar-flip");
+  }
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_elider(&s1, 5, 3);
+    CHECK(&s1, "hoojarip");
+  }
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_elider(&s1, 5, 7);
+    CHECK(&s1, "hoojar");
+  }
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_elider(&s1, 12, 7);
+    CHECK(&s1, "-flip");
+  }
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_elider(&s1, 12, 14);
+    CHECK(&s1, "");
+  }
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_relider(&s1, 5, 3);
+    CHECK(&s1, "hor-flip");
+  }
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_relider(&s1, 5, 7);
+    CHECK(&s1, "r-flip");
+  }
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_relider(&s1, 12, 7);
+    CHECK(&s1, "hooja");
+  }
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_relider(&s1, 12, 14);
+    CHECK(&s1, "");
+  }
+
+
+
+
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_elect(&s1, 3, 3);
+    CHECK(&s1, "jar");
+  }
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_elect(&s1, 3, 10);
+    CHECK(&s1, "jar-flip");
+  }
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_elect(&s1, 12, 10);
+    CHECK(&s1, "");
+  }
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_relect(&s1, 3, 3);
+    CHECK(&s1, "r-f");
+  }
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_relect(&s1, 3, 10);
+    CHECK(&s1, "hoojar-f");
+  }
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_relect(&s1, 12, 10);
+    CHECK(&s1, "");
+  }
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_electr(&s1, 5, 3);
+    CHECK(&s1, "-fl");
+  }
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_electr(&s1, 5, 7);
+    CHECK(&s1, "-flip");
+  }
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_electr(&s1, 12, 7);
+    CHECK(&s1, "hoojar");
+  }
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_electr(&s1, 12, 14);
+    CHECK(&s1, "hoojar-flip");
+  }
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_relectr(&s1, 5, 3);
+    CHECK(&s1, "oja");
+  }
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_relectr(&s1, 5, 7);
+    CHECK(&s1, "hooja");
+  }
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_relectr(&s1, 12, 7);
+    CHECK(&s1, "r-flip");
+  }
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_relectr(&s1, 12, 14);
+    CHECK(&s1, "hoojar-flip");
+  }
+
+
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_truncate(&s1, 7);
+    CHECK(&s1, "hoojar-");
+  }
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_truncate(&s1, 14);
+    CHECK(&s1, "hoojar-flip");
+  }
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_rtruncate(&s1, 7);
+    CHECK(&s1, "hooj");
+  }
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_rtruncate(&s1, 14);
+    CHECK(&s1, "");
+  }
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_neck(&s1, 7);
+    CHECK(&s1, "flip");
+  }
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_neck(&s1, 14);
+    CHECK(&s1, "");
+  }
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_rneck(&s1, 7);
+    CHECK(&s1, "ar-flip");
+  }
+
+  {
+    int rc = vstr_set(&s1, "hoojar-flip");
+    assert(rc == 0);
+    vstr_rneck(&s1, 14);
+    CHECK(&s1, "");
+  }
 
   return 0;
 }
