0b2f07553acb32e53fcb3ef8e7ed3d8bf6201628
Steven Simpson <568737+simpsonst@users.noreply.github.com>
2016-07-15 11:12:01 +0000

    Fixed listing of dependencies.

--- excluded-config.mk
+++ excluded-config.mk
@@ -1,12 +1,12 @@
 
 list-deps::
-	@$(PRINTF) '%s\n' GNU-make ISO-C90 Binodeps
+	@echo GNU-make ISO-C90 Binodeps
 
 list-opts::
-	@$(PRINTF) '%s\n' ISO-C99 ISO-C++11
+	@echo ISO-C99 ISO-C++11
 
 list-platforms::
-	@$(PRINTF) 'riscos'
+	@echo 'riscos'
 
 ddslib_zipdesc=$(cq)iflang($(oq2)eo$(cq2), $(oq2)Antaŭtradukita biblioteko$(cq2), $(oq2)Precompiled library$(cq2))$(oq)
 
