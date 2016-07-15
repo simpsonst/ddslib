c7a4a66e489799ed8b84fb38244b8ae333755ae5
Steven Simpson <568737+simpsonst@users.noreply.github.com>
2016-07-15 14:36:15 +0000

    Platform "linux" should be explicit to trigger native install.

--- excluded-config.mk
+++ excluded-config.mk
@@ -6,7 +6,7 @@ list-opts::
 	@echo ISO-C99 ISO-C++11
 
 list-platforms::
-	@echo 'riscos'
+	@echo linux riscos
 
 ddslib_zipdesc=$(cq)iflang($(oq2)eo$(cq2), $(oq2)Antaŭtradukita biblioteko$(cq2), $(oq2)Precompiled library$(cq2))$(oq)
 
