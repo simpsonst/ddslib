e41c0cc1828712af72e7c9bb31f214135812ede7
Steven Simpson <568737+simpsonst@users.noreply.github.com>
2016-07-18 16:38:56 +0000

    Using explicit exports.

--- excluded-config.mk
+++ excluded-config.mk
@@ -6,7 +6,10 @@ list-opts::
 	@echo ISO-C99 ISO-C++11
 
 list-platforms::
-	@echo linux windows riscos
+	@echo windows
+
+list-exports::
+	@echo linux riscos
 
 ddslib_zipdesc=$(cq)iflang($(oq2)eo$(cq2), $(oq2)Antaŭtradukita biblioteko$(cq2), $(oq2)Precompiled library$(cq2))$(oq)
 
